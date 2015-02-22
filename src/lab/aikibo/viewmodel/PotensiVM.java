651681c229a5b7b62fff20d14daf6a58cba35381
TAMAMI <TAMAMI@PBB-NB>
2015-02-22 22:15:38 +0700

    coba pake @DependsOn

--- src/lab/aikibo/viewmodel/PotensiVM.java
+++ src/lab/aikibo/viewmodel/PotensiVM.java
@@ -5,6 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.DependsOn;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.zk.ui.util.Clients;
@@ -153,6 +154,7 @@ public class PotensiVM {
 		}
 	}
 
+	@DependsOn("currentKecamatan")
 	public String getCurrentKelurahan() {
 		return currentKelurahan;
 	}
