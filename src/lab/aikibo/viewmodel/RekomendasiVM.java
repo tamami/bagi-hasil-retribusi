8c565315aafa57485c5c2eb3087c35d78d2d8023
TAMAMI <TAMAMI@PBB-NB>
2015-02-25 20:06:12 +0700

    pemanggilan report

--- src/lab/aikibo/viewmodel/RekomendasiVM.java
+++ src/lab/aikibo/viewmodel/RekomendasiVM.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
@@ -13,20 +15,31 @@ import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 
 import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
 import org.zkoss.util.media.AMedia;
 import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zul.Filedownload;
 import org.zkoss.zul.Iframe;
 
 public class RekomendasiVM {
+	
+	private List<String> daftarBulan;
+	private String currentBulan;
+	private String thnRekom;
+	
+	@Init
+	public void init() {
+		daftarBulan = new LinkedList<String>();
+		thnRekom = new String();
+	}
 	 
 	@Command
 	public void showReport() throws JRException, IOException {
-		JasperPrint print = JasperFillManager.fillReport("../webapps/ZK-Bagihasil-Retribusi/report/hello_report.jasper", null, new JREmptyDataSource());
-		JasperExportManager.exportReportToPdfFile(print, "../webapps/ZK-Bagihasil-Retribusi/report/hello_report.pdf");
+		JasperPrint print = JasperFillManager.fillReport("../webapps/ZK-Bagihasil-Retribusi/report/hello.jasper", null, new JREmptyDataSource());
+		JasperExportManager.exportReportToPdfFile(print, "../webapps/ZK-Bagihasil-Retribusi/report/hello.pdf");
 		
 		//Iframe iframe = (Iframe) this.getFellow("iframe");
-		File f = new File("../webapps/ZK-Bagihasil-Retribusi/report/hello_report.pdf");
+		File f = new File("../webapps/ZK-Bagihasil-Retribusi/report/hello.pdf");
 		byte[] buffer = new byte[(int) f.length()];
 		FileInputStream fs = new FileInputStream(f);
 		fs.read(buffer);
@@ -36,7 +49,29 @@ public class RekomendasiVM {
 		Filedownload.save(amedia);
 		//iframe.setContent(amedia);
 	}
-	
-	
+
+	public List<String> getDaftarBulan() {
+		return daftarBulan;
+	}
+
+	public void setDaftarBulan(List<String> daftarBulan) {
+		this.daftarBulan = daftarBulan;
+	}
+
+	public String getThnRekom() {
+		return thnRekom;
+	}
+
+	public void setThnRekom(String thnRekom) {
+		this.thnRekom = thnRekom;
+	}
+
+	public String getCurrentBulan() {
+		return currentBulan;
+	}
+
+	public void setCurrentBulan(String currentBulan) {
+		this.currentBulan = currentBulan;
+	}
 
 }
