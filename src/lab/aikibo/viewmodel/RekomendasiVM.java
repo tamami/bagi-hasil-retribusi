61f9e96a41c575aa6fb3fdc61b303eeecbddbe74
P. Tamami <tamami.oka@gmail.com>
2015-03-14 22:17:04 +0700

    bug fixed untuk report UI

--- src/lab/aikibo/viewmodel/RekomendasiVM.java
+++ src/lab/aikibo/viewmodel/RekomendasiVM.java
@@ -5,12 +5,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import lab.aikibo.entity.Rekomendasi;
 import lab.aikibo.manager.BulanManager;
+import lab.aikibo.manager.RekomendasiManager;
+import lab.aikibo.util.NumberFormatConverter;
 
 import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
@@ -18,8 +22,10 @@ import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 
+import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.util.media.AMedia;
 import org.zkoss.zk.ui.util.Clients;
 import org.zkoss.zul.Filedownload;
@@ -28,13 +34,22 @@ import org.zkoss.zul.Iframe;
 public class RekomendasiVM {
 	
 	private List<String> daftarBulan;
-	private String currentBulan;
+	private int currentBulan;
+	private String kodeBulan;
 	private String thnRekom;
 	private BulanManager bm;
+	private RekomendasiManager rm;
+	private List<Rekomendasi> daftarRekomendasiUi;
+	private List<Rekomendasi> daftarRekomendasi;
+	private Rekomendasi currentRekomendasi;
+	private NumberFormatConverter nfc;
 	
 	@Init
 	public void init() {
 		bm = new BulanManager();
+		rm = new RekomendasiManager();
+		setNfc(new NumberFormatConverter());
+		setDaftarRekomendasi(rm.getDaftarRekomendasi());
 		
 		clear();
 	}
@@ -42,6 +57,19 @@ public class RekomendasiVM {
 	public void clear() {
 		daftarBulan = bm.getFormattedDaftarBulan();
 		thnRekom = new String();
+		setCurrentBulan(0);
+		setThnRekom("");
+		initDaftarRekomendasiUi();
+	}
+	
+	private void initDaftarRekomendasiUi() {
+		daftarRekomendasiUi = new LinkedList<Rekomendasi>();
+		for(int i=0;i<daftarRekomendasi.size();i++) {
+			Rekomendasi r = daftarRekomendasi.get(i);
+			if(r != null) {
+				daftarRekomendasiUi.add(r);
+			}
+		}
 	}
 	 
 	@Command
@@ -65,8 +93,10 @@ public class RekomendasiVM {
 	}
 	
 	@Command 
-	public void prosesReport() {
-		
+	@NotifyChange({"daftarRekomendasiUi","daftarRekomendasi","currentBulan","thnRekom"})
+	public void prosesReport(@BindingParam("bulan")String bln, @BindingParam("tahun") String thn) {
+		daftarRekomendasi = rm.getDaftarByParam(bln, thn);
+		clear();
 	}
 	
 	
@@ -88,12 +118,64 @@ public class RekomendasiVM {
 		this.thnRekom = thnRekom;
 	}
 
-	public String getCurrentBulan() {
+	public int getCurrentBulan() {
 		return currentBulan;
 	}
 
-	public void setCurrentBulan(String currentBulan) {
+	public void setCurrentBulan(int currentBulan) {
+		kodeBulan = bm.getKodeBulanFromIndex(currentBulan);
 		this.currentBulan = currentBulan;
+	}
+
+	public RekomendasiManager getRm() {
+		return rm;
+	}
+
+	public void setRm(RekomendasiManager rm) {
+		this.rm = rm;
+	}
+
+	public List<Rekomendasi> getDaftarRekomendasi() {
+		return daftarRekomendasi;
+	}
+
+	public void setDaftarRekomendasi(List<Rekomendasi> daftarRekomendasi) {
+		this.daftarRekomendasi = daftarRekomendasi;
+	}
+
+	public Rekomendasi getCurrentRekomendasi() {
+		//if(currentRekomendasi.getRealGangguan() == null) currentRekomendasi.setRealGangguan(new BigDecimal("0"));
+		//if(currentRekomendasi.getRealImb() == null) currentRekomendasi.setRealImb(new BigDecimal("0"));
+		//if(currentRekomendasi.getRealLelang() == null) currentRekomendasi.setRealLelang(new BigDecimal("0"));
+		return currentRekomendasi;
+	}
+
+	public void setCurrentRekomendasi(Rekomendasi currentRekomendasi) {
+		this.currentRekomendasi = currentRekomendasi;
+	}
+	
+	public List<Rekomendasi> getDaftarRekomendasiUi() {
+		return daftarRekomendasiUi;
+	}
+	
+	public void setDaftarRekomendasiUi(List<Rekomendasi> daftarRekomendasiUi) {
+		this.daftarRekomendasiUi = daftarRekomendasiUi;  
+	}
+
+	public NumberFormatConverter getNfc() {
+		return nfc;
+	}
+
+	public void setNfc(NumberFormatConverter nfc) {
+		this.nfc = nfc;
+	}
+
+	public String getKodeBulan() {
+		return kodeBulan;
+	}
+
+	public void setKodeBulan(String kodeBulan) {
+		this.kodeBulan = kodeBulan;
 	}
 
 }
