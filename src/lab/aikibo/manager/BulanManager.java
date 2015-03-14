a1b6784cfd83515e44dda40e17348b21ed8e67b1
P. Tamami <tamami.oka@gmail.com>
2015-03-14 22:09:36 +0700

    penambahan method getKodeBulanFromIndex

--- src/lab/aikibo/manager/BulanManager.java
+++ src/lab/aikibo/manager/BulanManager.java
@@ -57,5 +57,23 @@ public class BulanManager {
 		default : return 0;
 		}
 	}
+	
+	public String getKodeBulanFromIndex(int index) {
+		switch(index) {
+		case 0: return "01";
+		case 1: return "02";
+		case 2: return "03";
+		case 3: return "04";
+		case 4: return "05";
+		case 5: return "06";
+		case 6: return "07";
+		case 7: return "08";
+		case 8: return "09";
+		case 9: return "10";
+		case 10: return "11";
+		case 11: return "12";
+		default : return null;
+		}
+	}
 
 }
